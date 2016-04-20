182d55294a5bec8eb3dfd56baa8e26eae753d73c
LetoDK <leanderholm@gmail.com>
2016-04-20 13:58:35 +0200

    - Works for me

--- src/DistributedTextEditor.java
+++ src/DistributedTextEditor.java
@@ -5,10 +5,6 @@ import java.io.*;
 import javax.swing.*;
 import javax.swing.text.*;
 import javax.swing.event.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.concurrent.*;
 
 public class DistributedTextEditor extends JFrame {
@@ -28,8 +24,6 @@ public class DistributedTextEditor extends JFrame {
     private boolean changed = false;
     private boolean connected = false;
     private DocumentEventCapturer dec = new DocumentEventCapturer();
-
-    protected ServerSocket serverSocket;
 
     public DistributedTextEditor() {
         area1.setFont(new Font("Monospaced",Font.PLAIN,12));
@@ -102,118 +96,25 @@ public class DistributedTextEditor extends JFrame {
     };
 
     Action Listen = new AbstractAction("Listen") {
-        public void actionPerformed(final ActionEvent e) {
+        public void actionPerformed(ActionEvent e) {
             saveOld();
             area1.setText("");
             // TODO: Become a server listening for connections on some port.
-            //Homemade code start
-            System.out.println("Hello world!");
-
-            printLocalHostAddress();
-
-            registerOnPort();
-
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        final Socket socket = waitForConnectionFromClient();
-
-                        if (socket != null) {
-                            System.out.println("Connection from " + socket);
-                            new Thread(new Runnable() {
-                                public void run() {
-                                    try {
-                                        // For reading from standard input
-                                        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-                                        // For sending text to the server
-                                        PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
-                                        String s;
-                                        // Read from standard input and send to server
-                                        // Ctrl-D terminates the connection
-                                        System.out.print("Type something for the server and then RETURN> ");
-                                        while ((s = stdin.readLine()) != null && !toServer.checkError()) {
-                                            System.out.print("Type something for the server and then RETURN> ");
-                                            toServer.println(s);
-                                        }
-                                        socket.close();
-                                    } catch (IOException e) {
-                                        // We ignore IOExceptions
-                                    }
-                                }
-                            }).start();
-
-                            try {
-                                BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                                String s;
-                                // Read and print what the client is sending
-                                while ((s = fromClient.readLine()) != null) { // Ctrl-D terminates the connection
-                                    area1.insert("From the client: " + s, 0);
-                                }
-                                socket.close();
-                            } catch (IOException ex) {
-                                // We report but otherwise ignore IOExceptions
-                                System.err.println(e);
-                            }
-                            System.out.println("Connection closed by client.");
-                        } else {
-                            // We rather agressively terminate the server on the first connection exception
-                            break;
-                        }
-                        //Homemade code end
-                        changed = false;
-                        Save.setEnabled(false);
-                        SaveAs.setEnabled(false);
-                    }
-                }
-            }).start();
-
-        }
-    };
-
-    protected String printLocalHostAddress() {
-        try {
-            InetAddress localhost = InetAddress.getLocalHost();
-            String localhostAddress = localhost.getHostAddress();
-            System.out.println("Contact this server on the IP address " + localhostAddress);
-            return localhostAddress;
-        } catch (UnknownHostException e) {
-            System.err.println("Cannot resolve the Internet address of the local host.");
-            System.err.println(e);
-            System.exit(-1);
-            return "fejl";
-        }
-    };
-
-    public void deregisterOnPort() {
-        if (serverSocket != null) {
+            Server server = new Server();
+            Thread serverThread = new Thread(server);
+            serverThread.start();
             try {
-                serverSocket.close();
-                serverSocket = null;
-            } catch (IOException e) {
-                System.err.println(e);
+                Thread.sleep(1000);                 //1000 milliseconds is one second.
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
-        }
-    };
-
-    protected Socket waitForConnectionFromClient() {
-        Socket res = null;
-        try {
-            res = serverSocket.accept();
-        } catch (Exception e) {
-
-        }
-        return res;
-    };
-
-    protected void registerOnPort() {
-        try {
-            serverSocket = new ServerSocket(Integer.parseInt(portNumber.getText()));
-        } catch (IOException e) {
-            serverSocket = null;
-            System.err.println("Cannot open server socket on port number" + Integer.parseInt(portNumber.getText()));
-            System.err.println(e);
-            System.exit(-1);
+            System.out.println("Server running");
+            String localhostAddress = server.localhostAddress;
+            System.out.println("localhost address: " + localhostAddress);
+            setTitle("I'm listening on " + localhostAddress);
+            changed = true;
+            Save.setEnabled(true);
+            SaveAs.setEnabled(true);
         }
     };
 
@@ -222,75 +123,20 @@ public class DistributedTextEditor extends JFrame {
             saveOld();
             area1.setText("");
             setTitle("Connecting to " + ipaddress.getText() + ":" + portNumber.getText() + "...");
-            changed = false;
-            Save.setEnabled(false);
-            SaveAs.setEnabled(false);
-            System.out.println("Hello world!");
-            System.out.println("Type CTRL-D to shut down the client.");
 
-            printLocalHostAddress();
-            final String serverName = ipaddress.getText();
-            final Socket socket = connectToServer(serverName);
-
-            if (socket != null) {
-                System.out.println("Connected to " + socket);
-                new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            BufferedReader fromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            String s;
-                            // Read and print what the client is sending
-                            while ((s = fromClient.readLine()) != null) { // Ctrl-D terminates the connection
-                                System.out.println("From the client: " + s);
-                            }
-                            socket.close();
-                        } catch (IOException e) {
-                            // We report but otherwise ignore IOExceptions
-                            System.err.println(e);
-                        }
-                    }}).start();
-
-                try {
-                    // For reading from standard input
-                    final BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-                    // For sending text to the server
-                    final PrintWriter toServer = new PrintWriter(socket.getOutputStream(),true);
-                    final String[] s = new String[1];
-                    // Read from standard input and send to server
-                    // Ctrl-D terminates the connection
-                    System.out.print("Type something for the server and then RETURN> ");
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                while ((s[0] = stdin.readLine()) != null && !toServer.checkError()) {
-                                    System.out.print("Type something for the server and then RETURN> ");
-                                    toServer.println(s[0]);
-                                }
-                            } catch (IOException e1) {
-                                e1.printStackTrace();
-                            }
-                        }
-                    }).start();
-
-                    socket.close();
-                } catch (IOException ex) {
-                    // We ignore IOExceptions
-                }
+            Client client = new Client(ipaddress.getText(), portNumber.getText());
+            Thread clientThread = new Thread(client);
+            clientThread.start();
+            try {
+                Thread.sleep(1000);                 //1000 milliseconds is one second.
+            } catch(InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
 
-
+            changed = true;
+            Save.setEnabled(true);
+            SaveAs.setEnabled(true);
         }
-    };
-
-    protected Socket connectToServer(String serverName) {
-        Socket res = null;
-        try {
-            res = new Socket(serverName,40604);
-        } catch (IOException e) {
-            // We return null on IOExceptions
-        }
-        return res;
     };
 
     Action Disconnect = new AbstractAction("Disconnect") {
--- src/Server.java
+++ src/Server.java
@@ -3,31 +3,32 @@ import java.io.*;
 
 /**
  *
- * A very simple server which will way for a connection from a client and print
+ * A very simple server which will way for a connection from a client and print 
  * what the client sends. When the client closes the connection, the server is
  * ready for the next client.
  */
 
-public class DemoServer {
+public class Server implements Runnable{
 
     /*
-     * Your group should use port number 40HGG, where H is your "hold nummer (1,2 or 3)
+     * Your group should use port number 40HGG, where H is your "hold nummer (1,2 or 3) 
      * and GG is gruppe nummer 00, 01, 02, ... So, if you are in group 3 on hold 1 you
      * use the port number 40103. This will avoid the unfortunate situation that you
      * connect to each others servers.
      */
-    protected int portNumber = 40604;
+    public int portNumber = 40604;
     protected ServerSocket serverSocket;
+    public String localhostAddress = "0";
 
     /**
      *
      * Will print out the IP address of the local host and the port on which this
-     * server is accepting connections.
+     * server is accepting connections. 
      */
     protected void printLocalHostAddress() {
         try {
             InetAddress localhost = InetAddress.getLocalHost();
-            String localhostAddress = localhost.getHostAddress();
+            localhostAddress = localhost.getHostAddress();
             System.out.println("Contact this server on the IP address " + localhostAddress);
         } catch (UnknownHostException e) {
             System.err.println("Cannot resolve the Internet address of the local host.");
@@ -65,7 +66,7 @@ public class DemoServer {
 
     /**
      *
-     * Waits for the next client to connect on port number portNumber or takes the
+     * Waits for the next client to connect on port number portNumber or takes the 
      * next one in line in case a client is already trying to connect. Returns the
      * socket of the connection, null if there were any failures.
      */
@@ -88,6 +89,7 @@ public class DemoServer {
 
         while (true) {
             Socket socket = waitForConnectionFromClient();
+            System.out.println("Socket recieved");
 
             if (socket != null) {
                 System.out.println("Connection from " + socket);
@@ -106,6 +108,7 @@ public class DemoServer {
                 System.out.println("Connection closed by client.");
             } else {
                 // We rather agressively terminate the server on the first connection exception
+                System.out.println("break");
                 break;
             }
         }

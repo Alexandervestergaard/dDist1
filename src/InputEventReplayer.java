3efc7109716720aad0efede0512f6698984e2173
LetoDK <leanderholm@gmail.com>
2016-04-22 11:08:30 +0200

    InputEvenReplayer

--- src/InputEventReplayer.java
+++ src/InputEventReplayer.java
@@ -1,5 +1,14 @@
-import javax.swing.JTextArea;
-import java.awt.EventQueue;
+/**
+ * Created by Alexander on 16-04-2016.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  *
@@ -10,31 +19,61 @@ import java.awt.EventQueue;
  * @author Jesper Buus Nielsen
  *
  */
-public class EventReplayer implements Runnable {
+public class InputEventReplayer implements Runnable {
 
     private DocumentEventCapturer dec;
     private JTextArea area;
+    public Socket socket;
+    private ObjectInputStream ois;
+    LinkedBlockingQueue<MyTextEvent> eventHistory;
 
-    public EventReplayer(DocumentEventCapturer dec, JTextArea area) {
+    public InputEventReplayer(DocumentEventCapturer dec, JTextArea area, Socket socket) {
         this.dec = dec;
         this.area = area;
+        this.socket = socket;
+        try {
+            ois = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        eventHistory = new LinkedBlockingQueue<MyTextEvent>();
+        startEventQueThread();
+    }
+
+    private void startEventQueThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        eventHistory.add((MyTextEvent) ois.readObject());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 
     public void run() {
         boolean wasInterrupted = false;
         while (!wasInterrupted) {
-            waitForOneSecond();
+            //waitForOneSecond();
             try {
-                MyTextEvent mte = dec.take();
+                MyTextEvent mte = eventHistory.take();
                 if (mte instanceof TextInsertEvent) {
                     final TextInsertEvent tie = (TextInsertEvent)mte;
                     EventQueue.invokeLater(new Runnable() {
                         public void run() {
                             try {
+
                                 area.insert(tie.getText(), tie.getOffset());
+
                             } catch (Exception e) {
                                 System.err.println(e);
-				    /* We catch all axceptions, as an uncaught exception would make the 
+				    /* We catch all axceptions, as an uncaught exception would make the
 				     * EDT unwind, which is now healthy.
 				     */
                             }
@@ -48,7 +87,7 @@ public class EventReplayer implements Runnable {
                                 area.replaceRange(null, tre.getOffset(), tre.getOffset()+tre.getLength());
                             } catch (Exception e) {
                                 System.err.println(e);
-				    /* We catch all axceptions, as an uncaught exception would make the 
+				    /* We catch all axceptions, as an uncaught exception would make the
 				     * EDT unwind, which is now healthy.
 				     */
                             }
